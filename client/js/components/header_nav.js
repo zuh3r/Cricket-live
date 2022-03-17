838aec36b16b3c76c137d90055a348629cca2676
kenwood <kennyp1995@gmail.com>
2022-03-17 16:41:26 +1100

    header nav changes

--- client/js/components/header_nav.js
+++ client/js/components/header_nav.js
@@ -1,10 +1,21 @@
 // Kenny created file 16/3
 function renderHeaderNav() {
     document.querySelector('#header-nav').innerHTML = `
-        <ul>
-            <li class="material-icons sign-up-icon" onClick="render('signUp')">person_add</li>
-            <li class="material-icons login-icon" onClick="render('login')">login</li>
-        </ul>
+        <header>
+            <nav class="left-nav">
+                <h2>Cricket-Live ⏚ <span id="welcome" class="welcome"></span></h2>
+            </nav>
+            <nav class="right-nav">
+                <ul>
+                    <li>News</li>
+                    <li>Matches</li>
+                    <li>Tournaments</li>
+                    <li>Players</li>
+                    <li onClick="render('signUp')">Sign Up</li>
+                    <li onClick="render('login')">Login</li>
+                </ul>
+            </nav>
+        </header>
     `
 }
 
