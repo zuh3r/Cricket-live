426b40d742d3408fe58b9d018ffedba11af26975
zvher <zuher.feki17@gmail.com>
2022-03-17 15:09:33 +1100

    updates

--- models/favourites.js
+++ models/favourites.js
@@ -1,26 +1,30 @@
 const db = require('../db/db')
 
-// const User = {
-//     findByEmail: email => {
-//         const sql = `
-//         SELECT * FROM users
-//         WHERE email = $1
-//         `
-//         return db
-//             .query(sql, [email])
-//             .then(dbRes => dbRes.rows[0])
-// },
+const Favourites = {
+    findByEmail: email => {
+        const sql = `
+        SELECT * FROM users
+        WHERE email = $1
+        `
+        return db
+            .query(sql, [email])
+            .then(dbRes => dbRes.rows[0])
+},
 
-//     create: (name, email, passwordDigest) => {
-//         const sql = `
-//           INSERT INTO users(name, email, password_digest)
-//           VALUES ($1, $2, $3)
-//           RETURNING *
-//         `
-//         return db
-//           .query(sql, [name, email, passwordDigest])
-//           .then(dbRes => dbRes.rows[0].name)
-//       }
-// }
+    findFavouritesByEmail: (email) => {
+        const sql = `
+          SELECT * FROM favourites
+          WHERE email = (
+            SELECT email FROM users WHERE email = $1
+          ) 
+          users(name, email, password_digest)
+          VALUES ($1, $2, $3)
+          RETURNING *
+        `
+        return db
+          .query(sql, [email])
+          .then(dbRes => dbRes.rows[0])
+      }
+}
     
-    module.exports = User
+    module.exports = Favourites
