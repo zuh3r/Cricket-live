98f8540412a62903aa7f5f77378a5a83e59de263
kenwood <kennyp1995@gmail.com>
2022-03-18 12:47:16 +1100

    welcome and my account changes

--- client/js/components/header_nav.js
+++ client/js/components/header_nav.js
@@ -1,9 +1,9 @@
 // Kenny created file 16/3
-function renderHeaderNav() {
+function renderHeaderNav(session = false) {
     document.querySelector('#header-nav').innerHTML = `
         <header>
             <nav class="left-nav">
-                <h2>Cricket-Live ⏚ <span id="welcome" class="welcome"></span></h2>
+                <h2>Cricket-Live ⏚ </span></h2>
             </nav>
             <nav class="right-nav">
                 <ul>
@@ -11,12 +11,17 @@ function renderHeaderNav() {
                     <li>Matches</li>
                     <li>Tournaments</li>
                     <li>Players</li>
-                    <li onClick="render('signUp')">Sign Up</li>
-                    <li onClick="render('login')">Login</li>
+                    <li id="welcome" class="welcome"></li>
+                    ${(!session)?
+                        `<li onClick="render('signUp')">Sign Up</li>
+                        <li onClick="render('login')">Login</li>`
+                        :``
+                    }
                 </ul>
             </nav>
         </header>
     `
+    renderFooter()
 }
 
 renderHeaderNav()
@@ -27,4 +32,17 @@ function render(component) {
     } else if (component === 'login') {
         renderLogin()
     }
+}
+
+function renderHomePage(){
+
+    document.querySelector('#page').innerHTML = `
+    <div class='newsSection'></div>
+    <div class='tournamentsSection'></div>
+    <div class='matchesSection'></div>
+    `
+
+    renderAllNews()
+    renderTournaments(tournaments,5)
+    renderMatches(matches)
 }
--- client/js/components/login.js
+++ client/js/components/login.js
@@ -21,20 +21,6 @@ function renderLogin() {
                 <button>Login</button>
             </form>
         </section>
-        <footer class="sign-up-footer">
-            <div class="footer-info">
-                <h4>cricket-Live is a production of APKZ Digital Media - Code4Cash.</h4>
-                <h4>© 2022 World Cricket.</h4>
-                <h4>All rights reserved.</h4>
-                <h4>Cricket-Live ⏚</h4>
-            </div>
-            <div class="footer-icons">
-                <span>Contact</span>
-                <span>Facebook</span>
-                <span>Instagram</span>
-                <span>Youtube</span>
-            </div>
-        </footer>
     `
 }
 
@@ -48,7 +34,10 @@ function login(event) {
     axios
         .post('/api/sessions', data)
         .then(res => res.data)
-        .then(user => renderWelcome(user.username, user.email))
+        .then(user => {
+            renderHeaderNav(true)
+            renderWelcome(user.username, user.email)
+        })
         .catch(error => {
             let errorDOM = document.querySelector('.login .error')
             errorDOM.textContent = error.response.data.message
